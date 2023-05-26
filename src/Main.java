3ca07f79c86420cec46968f35650b77b2a997565
Nazareno Pisoni <nazareno_pisoni@hotmail.com>
2023-05-26 13:12:21 -0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -15,26 +15,41 @@ public class Main {
         //pantalla, lanzar una excepción y contarlo como un intento. Investigar qué
         //excepción controla el tipo de datos ingresados.
 
-//        int numero = (int) (Math.random()*500+1);
-//        int numeroAdivinar = 0;
-//        int intentos = 0;
-//        do {
-//            try{
-//                System.out.println("Ingrese un numero entre 1 y 500 para ver si lo adivina: ");
-//                Scanner sc = new Scanner(System.in);
-//                numeroAdivinar = sc.nextInt();
-//            }catch (InputMismatchException e){
-//                System.out.println("Cuidado! Debe ingresar numeros enteros.");
-//                intentos++;
-//            }
-//            if(numeroAdivinar < numero){
-//                System.out.println("Debe ingresar un numero mayor.");
-//                intentos++;
-//            } else if (numeroAdivinar > numero) {
-//                System.out.println("Debe ingresar un numero menor.");
-//                intentos++;
-//            }else System.out.println("Ha adivinado el numero luego de " + intentos + " intentos.");
-//        }while (numeroAdivinar != numero);
+        int numero = (int) (Math.random()*500+1);
+        int numeroAdivinar = 0;
+        int intentos = 0;
+        do {
+            try{
+                System.out.println("Ingrese un numero entre 1 y 500 para ver si lo adivina: ");
+                Scanner sc = new Scanner(System.in);
+                numeroAdivinar = sc.nextInt();
+            }catch (InputMismatchException e){
+                System.out.println("Cuidado! Debe ingresar numeros enteros.");
+                intentos++;
+            }
+            if(numeroAdivinar < numero){
+                System.out.println("Debe ingresar un numero mayor.");
+                intentos++;
+            } else if (numeroAdivinar > numero) {
+                System.out.println("Debe ingresar un numero menor.");
+                intentos++;
+            }else System.out.println("Ha adivinado el numero luego de " + intentos + " intentos.");
+        }while (numeroAdivinar != numero);
+
+        //4. Crea una clase colegio que almacene el listado de los alumnos
+        //(compuestos por su nombre y nacionalidad). La clase tendrá los siguientes
+        //métodos:
+        //agregarAlumno(Alumno alumno): añade la nacionalidad de un nuevo
+        //alumno
+        //verNacionalidad(String nacionalidad): Muestra la nacionalidad y el número de
+        //alumnos de esa nacionalidad
+        //cuantos(): Muestra cuántas nacionalidades diferentes existen en el colegio.
+        //borrar(Alumno alumno): Elimina los datos insertados.
+        //VerTodos(): Muestra las distintas nacionalidades y el número de alumnos que
+        //existen por nacionalidad.
+        //Para todos estos métodos se deben generar distintas validaciones para prevenir
+        //errores en la ejecución. Por ej: Que todos los alumnos tengan una nacionalidad;
+        //Que exista el alumno que se quiere borrar, etc.
 
         Alumno a1 = new Alumno("Nazareno", "Argentino");
         Alumno a2 = new Alumno("Mikkel", "Aleman");
